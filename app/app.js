d30f0f238c21a113bf57a53ad0cda294cfb9d2e6
Andrew Horvath <skwerl@student.fullsail.edu>
2022-10-16 20:09:14 -0500

    added site files

--- app/app.js
+++ app/app.js
@@ -1,4 +1,25 @@
 const express = require('express');
 const app = express();
+const ejs = require('ejs');
+const morgan = require('morgan');
+const router = require('../routes/router');
+
+//use middleware to set payload and url encoding
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//middleware for logging
+app.use(morgan('dev'));
+
+//ejs middleware to set the template and the engine
+app.set('view engine', 'ejs');
+app.engine('ejs', ejs.__express);
+
+//declare static file locations
+app.use(express.static('view'));
+app.use(express.static('public'));
+
+//use middleware to set router
+app.use('/', router);
 
 module.exports = app;
