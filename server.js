152e64854f519a50888ce7abe520d6e504c3f70b
Andrew Horvath <skwerl@student.fullsail.edu>
2022-10-16 19:16:55 -0500

    added app and update server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const http = require('http');
 require('dotenv').config();
+const app = require('./app/app');
 
-http.createServer().listen(process.env.port, () => {
+http.createServer(app).listen(process.env.port, () => {
     console.log(`Server is running on ${process.env.port}`);
 });
+
